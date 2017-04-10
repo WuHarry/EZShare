356780f33d91248d3ed4fac5521f8470962d92bd
Harry Wu <harrywhy32@gmail.com>
2017-04-11 00:07:17 +1000

    code update

--- src/EZShare/Client.java
+++ src/EZShare/Client.java
@@ -30,9 +30,10 @@ public class Client {
 
     public static void main(String[] args) {
 
+        //Record the resource size if the resource is a file
         int resourceSize = 0;
         //Load log config file
-        try{
+        try {
             FileInputStream config = new FileInputStream("logger.properties");
             LogManager.getLogManager().readConfiguration(config);
 
@@ -58,13 +59,11 @@ public class Client {
             //output stream
             DataOutputStream output =
                     new DataOutputStream(socket.getOutputStream());
-
-
             if (Connection.debugSwitch) {
                 logger.info("Debug mode on");
                 logger.fine("[SENT] - " + commandJsonString);
             }
-            if(commandJsonString != null){
+            if (commandJsonString != null) {
                 output.writeUTF(commandJsonString);
                 output.flush();
             }
@@ -72,28 +71,7 @@ public class Client {
             while (true) {
                 if (input.available() > 0) {
                     if (hasResources) {
-                        try {
-                            FileOutputStream fileOutputStream = new
-                                    FileOutputStream("1.jpg");
-                            byte[] buffer = new byte[1024];
-                            int bytesLeft = resourceSize;
-                            while (bytesLeft > 0) {
-                                int read = input.read(buffer, 0, Math.min(bytesLeft, buffer.length));
-                                if (read == -1){
-                                    throw new EOFException("Unexpected end of data");
-                                }
-                                fileOutputStream.write(buffer, 0, read);
-                                bytesLeft -= read;
-                            }
-                            fileOutputStream.close();
-                            hasResources = false;
-                            if(Connection.debugSwitch){
-                                logger.info("Resource read successfully.");
-                            }
-                            System.out.println("Resource read successfully.");
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
+                        downloadResources(input,resourceSize);
                     } else {
                         String message = input.readUTF();
                         //check if debug mode is on
@@ -105,7 +83,6 @@ public class Client {
                     }
                 }
             }
-
         } catch (IOException e) {
             logger.warning("[ERROR] - Unexpected response");
         }
@@ -115,8 +92,9 @@ public class Client {
      * The method to check if there is a resource need to be stored to file.
      *
      * @param message the server returned message
+     * @return the resource size if there is a resource file otherwise return 0
      */
-    public static int checkResources(String message) {
+    private static int checkResources(String message) {
         if (isJSONValid(message)) {
             JsonParser parser = new JsonParser();
             JsonObject response = (JsonObject) parser.parse(message);
@@ -137,13 +115,50 @@ public class Client {
         }
     }
 
-    public static boolean isJSONValid(String jsonInString) {
+    /**
+     * The method to check whether a string is a valid json string
+     *
+     * @param jsonInString
+     * @return true for the string is a json string
+     */
+    private static boolean isJSONValid(String jsonInString) {
         Gson gson = new Gson();
         try {
             gson.fromJson(jsonInString, Object.class);
             return true;
-        } catch(com.google.gson.JsonSyntaxException ex) {
+        } catch (com.google.gson.JsonSyntaxException ex) {
             return false;
+        }
+    }
+
+    /**
+     * The method to download the resources
+     * @param input the DataInputStream
+     * @param resourceSize the size of the resource which need to be download
+     */
+    private static void downloadResources(DataInputStream input,int resourceSize){
+        try {
+            hasResources = false;
+            FileOutputStream fileOutputStream = new
+                    FileOutputStream("1.jpg");
+            byte[] buffer = new byte[1024];
+            int bytesLeft = resourceSize;
+            while (bytesLeft > 0) {
+                int read = input.read(buffer, 0, Math.min(bytesLeft, buffer.length));
+                if (read == -1) {
+                    throw new EOFException("Unexpected end of data");
+                }
+                fileOutputStream.write(buffer, 0, read);
+                bytesLeft -= read;
+            }
+            fileOutputStream.close();
+            if (Connection.debugSwitch) {
+                logger.info("Resource read successfully.");
+            }
+            System.out.println("Resource read successfully.");
+        } catch (IOException e) {
+            logger.warning("[ERROR] - Resources download failed!");
+            e.printStackTrace();
         }
     }
 }
