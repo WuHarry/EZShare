2e56d736742f19712d2b18f7d169b114ff4f36e2
Harry Wu <harrywhy32@gmail.com>
2017-04-13 21:03:09 +1000

    resource

---
+++ src/Resource/Resource.java
@@ -0,0 +1,16 @@
+package Resource;
+
+/**
+ * Created by Yahang Wu on 2017/4/13.
+ * COMP90015 Distributed System Project1 EZServer
+ * Resource Class
+ */
+public class Resource {
+    String name = "";
+    String description = "";
+    String[] tags;
+    String uri;
+    String channel = "";
+    String owner = "";
+    String ezserver = "EZShare";
+}
