74805144b08960474e4d6d601b4dbf533727875c
Ryan Naughton <ryan.naughton@outlook.com>
2017-04-16 06:28:18 +1000

    Added exceptions for error cases.

---
+++ src/exceptions/InvalidResourceException.java
@@ -0,0 +1,16 @@
+package exceptions;
+
+/**
+ * Thrown when a resource supplied by a client contains incorrect information which cannot
+ * be recovered from.
+ *
+ */
+public class InvalidResourceException extends Exception {
+
+	private static final long serialVersionUID = 3749904713237494907L;
+
+	public InvalidResourceException(String message){
+		super(message);
+	}
+	
+}
---
+++ src/exceptions/MissingComponentException.java
@@ -0,0 +1,20 @@
+package exceptions;
+
+/**
+ * Thrown when a command is sent by a client without all fields which are needed for this type
+ * of command.
+ *
+ */
+public class MissingComponentException extends Exception {
+
+	private static final long serialVersionUID = -1341167278866437350L;
+	
+	public MissingComponentException(){
+		super();
+	}
+	
+	public MissingComponentException(String message){
+		super(message);
+	}
+
+}
